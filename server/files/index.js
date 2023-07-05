b1a38b1f6f07fb87e67b36bd3a77de6eb1c99cca
yozhic <yozhic@users.noreply.github.com>
2023-07-05 15:26:30 +0300

    Add server/files/files via upload

---
+++ server/files/index.js
@@ -0,0 +1,28 @@
+
+function getUrlParam (name) {
+  const results = new RegExp('[?&]' + name + '=(\\w+\\.[a-z]+(?:#[a-z]+)?)', 'i').exec(
+    window.location.href
+  )
+
+
+  if (results === null) {
+    return undefined
+  } else {
+    if (results[1] && decodeURIComponent(results[1]).trim() !== '') {
+      return decodeURIComponent(results[1]).trim()
+    } else {
+      return undefined
+    }
+  }
+}
+
+
+function LoadPage() {
+
+  const page = getUrlParam('page')
+
+  if (page) {
+    document.getElementById('content').src = page
+  }
+
+}
